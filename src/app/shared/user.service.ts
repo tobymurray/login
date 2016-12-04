4e57adfe4749588588395b41351d01b3f926dd44
Toby Murray <murray.toby@gmail.com>
2016-12-04 17:53:26 -0500

    Use localStorage to persist token

--- src/app/shared/user.service.ts
+++ src/app/shared/user.service.ts
@@ -3,16 +3,21 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class UserService {
-
+  
+  private authToken = 'auth_token';
   private loggedIn: boolean = false;
 
-  constructor(private http: Http) { }
+  constructor(private http: Http) { 
+    this.loggedIn = !!localStorage.getItem(this.authToken);
+  }
 
   logIn(email: string, password: string) {
+    localStorage.setItem(this.authToken, 'token');
     this.loggedIn = true;
   }
 
   logOut() {
+    localStorage.removeItem(this.authToken);
     this.loggedIn = false;
   }
 
